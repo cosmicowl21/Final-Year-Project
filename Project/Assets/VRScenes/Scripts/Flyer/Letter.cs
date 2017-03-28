e332b02fd46b8dc681d691c62d11398f5a661aa7
James Walsh <jameswalsh6661@hotmail.com>
2017-03-28 10:00:30 +0100

    First Commit for 4th Year Project

---
+++ Project/Assets/VRScenes/Scripts/Flyer/Letter.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using UnityEngine;
+using VRStandardAssets.Common;
+using Random = UnityEngine.Random;
+
+namespace VRStanderdAssets.Flyer
+{
+    // This class controls each word in hte Flyer scene
+public class Letter : MonoBehaviour
+ {
+        // Event triggers for the Letters
+        public event Action<Letter> onLetterRemovalDistance;                   // This event triggers when the word if far enoug behind the camera
+        public event Action<Letter> onLetterCollide;                           // This event triggers when the ship hits the Letter.   
+
+        // Setting up all the values for the letters/ words
+        [SerializeField] private float m_LettersMinSize = 10f;                 // The Miniuim ammount the leters can be scaled up. 
+        [SerializeField] private float m_LettersMaxSize = 20f;                // The Maxiuim ammount the leters can be scaled up. 
+        [SerializeField] private float m_MinSpeed = 0f;                   // The Miniuim speed that the letter can move towards the camera 
+        [SerializeField] private float m_MaxSpeed = 10;                  // The Maxiuim speed that the letter can moves towards the camera
+        [SerializeField] private float m_MinRotationSpeed = 60f;        // The miniuim speed that the letter will rotate.
+        [SerializeField] private float m_MaxRotationSpeed = 140f;      //  The Maxuimin speed that the letter will rotate.
+
+        // setting up the references to diffrent objects 
+        private Rigidbody m_RigidBody;                                 // reference to the word rigid body, used to move and rotate it.
+        private GameObject m_Flyer;                                    // Referance to the flyer its self.
+        private Transform m_Camera;                                    // Referance to the camera
+        private float m_Speed;                                         // How fast the letters will move towards the camera.
+        private Vector3 m_RotationAxis;                                // The Axis around the letter.
+        private float m_RotationSpeed;                                 // how fast the letters will rotate.
+
+        private const float k_RemovalDistance = 50f;                    // How far behind the camera the letter can go before being destroyed.
+
+        public Action<Letter> OnLetterRemovalDistance { get; internal set; }
+        public Action<Letter> OnLetterHit { get; internal set; }
+
+        void Awake ()
+        {
+            m_RigidBody = GetComponent<Rigidbody>();
+            m_Camera = Camera.main.transform;
+        }
+	
+	// Update is called once per frame
+      private void Start ()
+      {
+            // set a random rotation for the letters of the word
+            transform.rotation = Random.rotation;
+
+            // set a random spin for the letters
+            m_Speed = Random.Range(m_MinSpeed, m_MaxSpeed);
+
+            // set up a random spin for the leters
+            m_RotationAxis = Random.insideUnitSphere;
+            m_RotationSpeed = Random.Range(m_MinRotationSpeed, m_MaxRotationSpeed);
+      }
+
+    private void Update()
+    {
+            // move and rotate the letters to the given parameters
+            m_RigidBody.MoveRotation(m_RigidBody.rotation * Quaternion.AngleAxis(m_RotationSpeed * Time.deltaTime, m_RotationAxis));
+            m_RigidBody.MovePosition(m_RigidBody.position - Vector3.forward * m_Speed * Time.deltaTime);
+
+            // if the letter is far enough behind the camera and something has subscribed to onwordremoval call it 
+            if(transform.position.z < m_Camera.position.z - k_RemovalDistance)
+            {
+                if (onLetterRemovalDistance != null)
+                    onLetterRemovalDistance(this);
+            }
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+            // only continue if the letter has hit the player (flyer)
+            if (other.gameObject != m_Flyer)
+            {
+                return;
+            }
+    }
+
+   
+    private void OnRemoval()
+    {
+            // ensure the events are completly removed when the letter is destroyed
+            onLetterRemovalDistance = null;
+            onLetterCollide = null;
+    }
+    
+    public void hit()
+    {
+            if (onLetterCollide != null)
+                onLetterCollide(this);
+    }
+}       
+
+}
---
+++ Project/Assets/VRScenes/Scripts/Flyer/LetterRemoval.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+namespace VRStanderdAssets.Flyer
+{ 
+    // This script looks after the explosion after a letter has been hit.
+public class LetterRemoval : MonoBehaviour
+{
+        public event Action<LetterRemoval> onRemovalEnded;                    // This event is triggerd after the last particle system has finnished.
+
+
+        private ParticleSystem[] m_ParticleSystems;                         // The particle systems for all the explosion and their children.
+        private float m_Duration;                                           // The longest duration of all the particle systems.                
+            
+	// Use this for initialization
+	void Awake ()
+    {
+            // try and find all the particle systems 
+            m_ParticleSystems = GetComponentsInChildren<ParticleSystem>(true);
+
+            // by default the duration is zero.
+            m_Duration = 0f;
+
+            // go through all the particle systems and if their duration is longer use that instead
+            for (int i = 0; i<m_ParticleSystems.Length; i++)
+            {
+                if (m_ParticleSystems[i].duration > m_Duration)
+                    m_Duration = m_ParticleSystems[i].duration;
+            }
+		
+	}
+	
+	// Update is called once per frame
+	private void OnDestory ()
+    {
+            onRemovalEnded = null;
+	}
+    
+    public void Restart()
+    {
+       // go through all the particle system and clear their current particles then play them
+       for (int i=0; i<m_ParticleSystems.Length;i++)
+       {
+                m_ParticleSystems[i].Clear();
+                m_ParticleSystems[i].Play();
+       }
+
+            // Start the timeout
+            StartCoroutine(Timeout());
+    }
+
+   
+    private IEnumerator Timeout()
+    {
+            // Wait for the longest particle system to finish
+            yield return new WaitForSeconds(m_Duration);
+
+            // If anything is subscribed to OnRemovalEnded call it
+            if (onRemovalEnded != null)
+                onRemovalEnded(this);
+
+    }
+
+
+}
+
+}
